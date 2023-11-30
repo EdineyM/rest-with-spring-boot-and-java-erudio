65ae624eef0736dbf7cd145a7f8a72c33cd250ba
Ediney Mendonça <edineyjr1@gmail.com>
2023-11-30 15:37:33 -0300

    First Steps in Java with Spring Boot 3

---
+++ rest-with-spring-boot-and-java-emsj/src/test/java/br/dev/edineysilva/StartupTests.java
@@ -0,0 +1,13 @@
+package br.dev.edineysilva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
